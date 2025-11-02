9b1bc81a76217a91c4ab03cbf47a6facc132e746
lucas-abalos <lucas22abalos@gmail.com>
2025-11-02 18:31:26 -0300

    Elimino controller viejo

--- e-commerce/src/main/java/SneakerCompany/e_commerce/controller/CatalogoProductosController.java
+++ e-commerce/src/main/java/SneakerCompany/e_commerce/controller/CatalogoProductosController.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +19,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/catalogo")
-@PreAuthorize("hasAnyRole('USER','ADMIN')")
 public class CatalogoProductosController {
 
     private final CatalogoProductosService catalogoProductosService;
